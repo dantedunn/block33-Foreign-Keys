7c990f899995b0f9db43dab8792be08706a78722
dantedunn <dantedunn@gmail.com>
2025-04-22 23:37:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const pg = require('pg');
+const express = require('express');
+const app = express();
+const { Client } = pg
+const port = 3000;//the port you want to run the server on, will be referenced in url
+ const client = new Client({
+    user: 'postgres',//the username you use to login to postico
+    password: 'shitbrick',//the password you use to login to postico
+    host: 'localhost',
+    port: 5432,//default port for postgres
+    database: 'postgres'//the name of the database you want to connect to from postico
+})
+app.listen(port, async () => {
+    await client.connect()//connect to the database
+    console.log(`Example app listening on port ${port}`)
+})
