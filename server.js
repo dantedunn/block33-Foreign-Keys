421fc9976d5efd2126ff4494a4b435d7caa29c1d
dantedunn <dantedunn@gmail.com>
2025-04-23 22:33:33 -0400

    added code

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const { Client } = pg
 const port = 3000;//the port you want to run the server on, will be referenced in url
+client.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Connection error:', err.message);
+  });
+
+
  const client = new Client({
     user: 'postgres',//the username you use to login to postico
     password: 'shitbrick',//the password you use to login to postico
@@ -11,13 +20,25 @@ const port = 3000;//the port you want to run the server on, will be referenced i
     database: 'postgres'//the name of the database you want to connect to from postico
 })
 
-app.get('/api/staff', async (req, res) => {
-    const result = await client.query('SELECT * FROM staff');
+app.get('/api/employee', async (req, res) => {
+    try {
+      const result = await client.query('SELECT * FROM employee');
+      res.json(result.rows); // Send the rows as a JSON response
+      console.log(result.rows); // Log the rows to the console
+    } catch (error) {
+      console.error(error); // Log the error
+      res.status(500).send('Error querying the database'); // Send a 500 error response
+    }
+  });
+
+  
+app.get('/api/departments', async (req, res) => {
+    const result = await client.query('SELECT * FROM departments');
     res.json(result.rows);
-    // console.log(result.rows);
+    console.log(result.rows);
 })
 
-
+app.post('/api/employee', async (req, res) => {
 
 
 
